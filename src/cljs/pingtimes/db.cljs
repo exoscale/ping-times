335fdfb51bd5e85dcbe32879803c7949d4867e22
Aaron Groom <ag644@cornell.edu>
2018-11-06 09:47:33 -0500

    Change ZH-1 to ZRH-1

--- src/cljs/pingtimes/db.cljs
+++ src/cljs/pingtimes/db.cljs
@@ -1,17 +1,17 @@
 (ns pingtimes.db)
 
 (def default-db
-  {:ping       {:ch-zh-1  {:latency 0 :initial? false}
+  {:ping       {:ch-zrh-1  {:latency 0 :initial? false}
                 :de-fra-1 {:latency 0 :initial? false}
                 :ch-gva-2 {:latency 0 :initial? false}
                 :at-vie-1 {:latency 0 :initial? false}}
-   :endpoints  {:ch-zh-1  "https://ping-times.sos-ch-dk-2.exo.io/index.html" ;; FIXME, once host is up
+   :endpoints  {:ch-zrh-1  "https://ping-times.sos-ch-dk-2.exo.io/index.html" ;; FIXME, once host is up
                 :de-fra-1 "https://ping-times-fra.sos-de-fra-1.exo.io/index.html"
                 :ch-gva-2 "https://ping-times.sos-ch-dk-2.exo.io/index.html" ;; FIXME, once host is up
                 :at-vie-1 "https://ping-times-vie.sos-at-vie-1.exo.io/index.html"}
    :detail-zone nil
-   :details    {:ch-zh-1 {:title "Zurich Datacenter"
-                          :content "Designed for scale, CH-ZH-1 is the latest datacenter from Equinix in Switzerland, built with state-of-the-art technologies, it is also home to Switzerland's densest network exchange point."
+   :details    {:ch-zrh-1 {:title "Zurich Datacenter"
+                          :content "Designed for scale, CH-ZRH-1 is the latest datacenter from Equinix in Switzerland, built with state-of-the-art technologies, it is also home to Switzerland's densest network exchange point."
                           :img "dc-ch-zh-1.png"}
                 :de-fra-1 {:title "Frankfurt Datacenter"
                            :content "The German DE-FRA-1 datacenter is ideally placed to serve teams looking for GDPR-compliant cloud servers. Hosted by the Equinix datacenter facility."
